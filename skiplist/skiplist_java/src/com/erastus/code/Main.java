7e5f8aea5638b48ef1703ffb373dc0720670abf2
Erastus Murungi <erastuswagwan@gmail.com>
2020-05-08 09:36:49 -0400

    Update Main.java

--- skiplist/skiplist_java/src/com/erastus/code/Main.java
+++ skiplist/skiplist_java/src/com/erastus/code/Main.java
@@ -1,4 +1,5 @@
 package com.erastus.code;
+
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -11,22 +12,35 @@ public class Main {
         int N = 1000000;
         int low = 10;
         int high = 50;
-	    List<Integer> randIntegers = Stream.iterate(0,
+        List<Integer> randIntegers = Stream.iterate(0,
                 n -> (int) (low + Math.random() * (high - low))).limit(N).collect(Collectors.toList());
 
-	    skipList<Integer> st = new skipList<>(Integer.MAX_VALUE);
+        skipList<Integer> st = new skipList<>(Integer.MAX_VALUE);
 
         Long[] times = new Long[N];
         long startTime;
         int i = 0;
-	    for (Integer n : randIntegers){
-	        startTime = System.nanoTime();
-	        st.insert(n);
-	        times[i++] = (System.nanoTime() - startTime);
+        for (Integer n : randIntegers) {
+            startTime = System.nanoTime();
+            st.insert(n);
+            times[i++] = (System.nanoTime() - startTime);
         }
-	    double mean = (double) (Arrays.stream(times).reduce((long) 0, Long::sum)) / N;
-        double sumProd = Arrays.stream(times).map((x) -> (x - mean) * (x - mean)).reduce( 0.0, Double::sum);
+        double mean = (double) (Arrays.stream(times).reduce((long) 0, Long::sum)) / N;
+        double sumProd = Arrays.stream(times).map((x) -> (x - mean) * (x - mean)).reduce(0.0, Double::sum);
         double stdDev = Math.sqrt(sumProd / N);
-        System.out.printf("%d skiplist.inserts took: %.2f \u00B1 %.2f \u00B5s", N, mean/1e3, stdDev/1e3);
+        System.out.printf("skiplist.insertt time: %.2f \u00B1 %.2f \u00B5s \n", mean / 1e3, stdDev / 1e3);
+
+        i = 0;
+        for (Integer n : randIntegers) {
+            startTime = System.nanoTime();
+            st.contains(n);
+            times[i++] = (System.nanoTime() - startTime);
+        }
+        double mean1 = (double) (Arrays.stream(times).reduce((long) 0, Long::sum)) / N;
+        sumProd = Arrays.stream(times).map((x) -> (x - mean1) * (x - mean1)).reduce(0.0, Double::sum);
+        stdDev = Math.sqrt(sumProd / N);
+        System.out.printf("skiplist.search time: %.2f \u00B1 %.2f ns", mean1, stdDev);
+
     }
 }
+=
