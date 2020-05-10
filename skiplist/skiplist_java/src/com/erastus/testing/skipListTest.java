01b20b87579d652de9fddcfffc2681309e57c394
Erastus Murungi <erastuswagwan@gmail.com>
2020-05-10 00:10:17 -0400

    Update and rename skipListTest to skipListTest.java

--- skiplist/skiplist_java/src/com/erastus/testing/skipListTest.java
+++ skiplist/skiplist_java/src/com/erastus/testing/skipListTest.java
@@ -38,6 +38,7 @@ class skipListTest {
         for (Integer index : shuffledIndices) {
             assertTrue(stInt.contains(expected1[index]));
         }
+        assertFalse(stInt.contains(100));
         stInt.clear();
         assertTrue(stInt.isEmpty());
 
@@ -88,7 +89,7 @@ class skipListTest {
             stInt.insert(n);
 
         assertEquals(98, stInt.nextLarger(83));
-        assertEquals(stInt.dummyInfinity(), stInt.nextLarger(98));
+        assertEquals(stInt.getBound(), stInt.nextLarger(98));
         assertEquals(83, stInt.nextLarger(74));
         assertEquals(61, stInt.nextLarger(48));
         assertEquals(48, stInt.nextLarger(42));
@@ -103,7 +104,7 @@ class skipListTest {
             stInt.insert(n);
 
         assertEquals(83, stInt.nextSmaller(98));
-        assertEquals(98, stInt.nextSmaller(stInt.dummyInfinity()));
+        assertEquals(98, stInt.nextSmaller(stInt.getBound()));
         assertEquals(74, stInt.nextSmaller(83));
         assertEquals(48, stInt.nextSmaller(61));
         assertEquals(42, stInt.nextSmaller(48));
@@ -222,5 +223,107 @@ class skipListTest {
         Collections.sort(expectedDoubles);
 
         assertEquals(expectedDoubles, sortedDoubles);
+    }
+
+
+    @org.junit.jupiter.api.Test
+    void splitTest() throws Exception {
+        Double[] values = {9.7, 10.8, 0.0, 30.6, 42.1, 48.5, 61.66};
+        skipList<Double> stDouble = new skipList<>(Double.POSITIVE_INFINITY);
+
+        for (Double val: values){
+            stDouble.insert(val);
+        }
+
+        skipList<Double> stLarge = stDouble.split(40.0);
+        Double[] smaller = {9.7, 10.8, 0.0, 30.6};
+        Double[] larger = {42.1, 48.5, 61.66};
+
+        for (Double small: smaller){
+            assertTrue(stDouble.contains(small));
+        }
+
+        for (Double large: larger) {
+            assertFalse(stDouble.contains(large));
+        }
+
+        for (Double small: smaller){
+            assertFalse(stLarge.contains(small));
+        }
+
+        for (Double large: larger) {
+            assertTrue(stLarge.contains(large));
+        }
+
+        assertEquals(30.6, stDouble.getMax());
+        assertEquals(42.1, stLarge.getMin());
+        assertEquals(3, stLarge.length());
+        assertEquals(4, stDouble.length());
+
+
+        stDouble.concatenate(stLarge);
+        for (Double small: smaller){
+            assertTrue(stDouble.contains(small));
+        }
+
+        for (Double large: larger) {
+            assertTrue(stDouble.contains(large));
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    void mergeTest() throws Exception {
+        Double[] X = {0.88, 0.83, 0.92, 0.71, 0.44, 0.87, 0.15, 0.34, 0.46, 0.99, 0.01, 0.33};
+        Double[] Y = {0.81, 0.99, 0.99, 0.99, 0.22, 0.7 , 0.02, 0.24, 0.87, 0.48, 0.54, 0.19, 0.45, 0.92};
+
+        skipList<Double> st1 = new skipList<>(Double.POSITIVE_INFINITY);
+        skipList<Double> st2 = new skipList<>(Double.POSITIVE_INFINITY);
+
+        for (Double s: X)
+            st1.insert(s);
+
+        for (Double s: Y)
+            st2.insert(s);
+
+        skipList<Double> st = st1.merge(st2, false);
+        for (Double s: Y)
+            assertTrue(st.contains(s));
+
+        Random rand = new Random();
+        double[] X1 = rand.doubles(1000).toArray();
+        double[] Y1 = rand.doubles(1000).toArray();
+
+        skipList<Double> st3 = new skipList<>(Double.POSITIVE_INFINITY);
+        skipList<Double> st4 = new skipList<>(Double.POSITIVE_INFINITY);
+
+        for (Double s: X1)
+            st3.insert(s);
+
+        for (Double s: Y1)
+            st4.insert(s);
+
+        skipList<Double> st5 = st3.merge(st4, false);
+
+        for (Double s: Y1)
+            assertTrue(st5.contains(s));
+
+
+        int[] X2 = rand.ints(100000).toArray();
+        int[] Y2 = rand.ints(100000).toArray();
+
+        skipList<Integer> st6 = new skipList<>(Integer.MAX_VALUE);
+        skipList<Integer> st7 = new skipList<>(Integer.MAX_VALUE);
+
+        for (Integer s: X2)
+            st6.insert(s);
+
+        for (Integer s: Y2)
+            st7.insert(s);
+
+        skipList<Integer> st8 = st6.merge(st7, false);
+
+        for (Integer s: Y2)
+            assertTrue(st8.contains(s));
+
     }
 }
