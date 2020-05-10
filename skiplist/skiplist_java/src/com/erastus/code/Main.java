4b3ba151bc4516a2a043ddc710ff207d88587f2d
Erastus Murungi <erastuswagwan@gmail.com>
2020-05-10 13:09:46 -0400

    Update Main.java

--- skiplist/skiplist_java/src/com/erastus/code/Main.java
+++ skiplist/skiplist_java/src/com/erastus/code/Main.java
@@ -15,7 +15,7 @@ public class Main {
         List<Integer> randIntegers = Stream.iterate(0,
                 n -> (int) (low + Math.random() * (high - low))).limit(N).collect(Collectors.toList());
 
-        skipList<Integer> st = new skipList<>(Integer.MAX_VALUE);
+        SkipList<Integer> st = new SkipList<>(Integer.MAX_VALUE);
 
         Long[] times = new Long[N];
         long startTime;
@@ -28,7 +28,7 @@ public class Main {
         double mean = (double) (Arrays.stream(times).reduce((long) 0, Long::sum)) / N;
         double sumProd = Arrays.stream(times).map((x) -> (x - mean) * (x - mean)).reduce(0.0, Double::sum);
         double stdDev = Math.sqrt(sumProd / N);
-        System.out.printf("skiplist.insertt time: %.2f \u00B1 %.2f \u00B5s \n", mean / 1e3, stdDev / 1e3);
+        System.out.printf("insert time: %.2f \u00B1 %.2f \u00B5s \n", mean / 1e3, stdDev / 1e3);
 
         i = 0;
         for (Integer n : randIntegers) {
@@ -39,8 +39,15 @@ public class Main {
         double mean1 = (double) (Arrays.stream(times).reduce((long) 0, Long::sum)) / N;
         sumProd = Arrays.stream(times).map((x) -> (x - mean1) * (x - mean1)).reduce(0.0, Double::sum);
         stdDev = Math.sqrt(sumProd / N);
-        System.out.printf("skiplist.search time: %.2f \u00B1 %.2f ns", mean1, stdDev);
+        System.out.printf("search time: %.2f \u00B1 %.2f ns\n", mean1, stdDev);
 
+        Double[] X = {0.88, 0.83, 0.92, 0.71, 0.81, 0.99, 0.22, 0.7 };
+        SkipList<Double> stDouble = new SkipList<>(Double.POSITIVE_INFINITY);
+        for (Double x: X){
+            stDouble.insert(x);
+        }
+        System.out.println(Colors.GREEN_BRIGHT);
+        stDouble.forEach(x -> System.out.println(x+x));
+        System.out.println(Colors.RESET);
     }
 }
-=
